5e7654e550109b35ce812af878055dd3ada05891
lioness3 <jcnailtech3@yahoo.com>
2020-01-20 12:11:50 -0800

    add capybara

---
+++ spec/features/add_product_pages_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe "the add product process" do
+  it "adds a new product" do
+    visit products_path
+    click_link 'New Product'
+    fill_in 'Name', :with => 'SHMOOOOO'
+    fill_in 'cost', :with => '33.33'
+    click_on 'Submit'
+    expect(page).to have_content 'Product successfully added!'
+    expect(page).to have_content 'SHMOOOOO'
+  end
+
+  it "gives an error when no name is entered" do
+    visit new_product_path
+    click_on 'New Product'
+    expect(page).to have_content "Name can't be blank"
+  end
+end
