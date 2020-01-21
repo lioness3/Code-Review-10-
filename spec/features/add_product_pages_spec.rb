a63520051904dc2e586c111c40b89dd733b578f8
lioness3 <jcnailtech3@yahoo.com>
2020-01-21 12:23:10 -0800

    get all tests to pass

--- spec/features/add_product_pages_spec.rb
+++ spec/features/add_product_pages_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe "the add product process" do
   it "gives an error when no name is entered" do
     visit '/products/new'
-    click_on 'Submit'
+    click_on 'Create Product'
     expect(page).to have_content "Name can't be blank"
   end
 end
